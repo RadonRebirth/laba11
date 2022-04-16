f9a5f7d9f36cd939df564ee9dc50e2275127dd7e
радон <96480522+RadonRebirth@users.noreply.github.com>
2022-04-16 10:35:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите IP адрес: ");
         String ip = in.nextLine();
-        Pattern pattern = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        Pattern pattern = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
         Matcher m = pattern.matcher(ip);
         if(m.find()){
             System.out.println("Верный IP адрес: ");
